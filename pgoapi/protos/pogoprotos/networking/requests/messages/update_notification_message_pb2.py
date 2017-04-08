4b0e0671ddfcba3b037002f7a8de6d2a63cd229b
Chrales <chrales@gmail.com>
2017-04-08 23:18:41 +0200

    quick fixes in new protos

--- pgoapi/protos/pogoprotos/networking/requests/messages/update_notification_message_pb2.py
+++ pgoapi/protos/pogoprotos/networking/requests/messages/update_notification_message_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='pogoprotos/networking/requests/messages/update_notification_message.proto',
   package='pogoprotos.networking.requests.messages',
   syntax='proto3',
-  serialized_pb=_b('\nIpogoprotos/networking/requests/messages/update_notification_message.proto\x12\'pogoprotos.networking.requests.messages\x1a)pogoprotos/enums/notification_state.proto\"\x83\x01\n\x16\x41\x64\x64\x46ortModifierMessage\x12\x18\n\x10notification_ids\x18\x01 \x03(\t\x12\x1b\n\x13\x63reate_timestamp_ms\x18\x02 \x03(\x03\x12\x32\n\x05state\x18\x03 \x01(\x0e\x32#.pogoprotos.enums.NotificationStateb\x06proto3')
+  serialized_pb=_b('\nIpogoprotos/networking/requests/messages/update_notification_message.proto\x12\'pogoprotos.networking.requests.messages\x1a)pogoprotos/enums/notification_state.proto\"\x86\x01\n\x19UpdateNotificationMessage\x12\x18\n\x10notification_ids\x18\x01 \x03(\t\x12\x1b\n\x13\x63reate_timestamp_ms\x18\x02 \x03(\x03\x12\x32\n\x05state\x18\x03 \x01(\x0e\x32#.pogoprotos.enums.NotificationStateb\x06proto3')
   ,
   dependencies=[pogoprotos_dot_enums_dot_notification__state__pb2.DESCRIPTOR,])
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
@@ -28,29 +28,29 @@ _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 
 
-_ADDFORTMODIFIERMESSAGE = _descriptor.Descriptor(
-  name='AddFortModifierMessage',
-  full_name='pogoprotos.networking.requests.messages.AddFortModifierMessage',
+_UPDATENOTIFICATIONMESSAGE = _descriptor.Descriptor(
+  name='UpdateNotificationMessage',
+  full_name='pogoprotos.networking.requests.messages.UpdateNotificationMessage',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='notification_ids', full_name='pogoprotos.networking.requests.messages.AddFortModifierMessage.notification_ids', index=0,
+      name='notification_ids', full_name='pogoprotos.networking.requests.messages.UpdateNotificationMessage.notification_ids', index=0,
       number=1, type=9, cpp_type=9, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='create_timestamp_ms', full_name='pogoprotos.networking.requests.messages.AddFortModifierMessage.create_timestamp_ms', index=1,
+      name='create_timestamp_ms', full_name='pogoprotos.networking.requests.messages.UpdateNotificationMessage.create_timestamp_ms', index=1,
       number=2, type=3, cpp_type=2, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='state', full_name='pogoprotos.networking.requests.messages.AddFortModifierMessage.state', index=2,
+      name='state', full_name='pogoprotos.networking.requests.messages.UpdateNotificationMessage.state', index=2,
       number=3, type=14, cpp_type=8, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
@@ -69,18 +69,18 @@ _ADDFORTMODIFIERMESSAGE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=162,
-  serialized_end=293,
+  serialized_end=296,
 )
 
-_ADDFORTMODIFIERMESSAGE.fields_by_name['state'].enum_type = pogoprotos_dot_enums_dot_notification__state__pb2._NOTIFICATIONSTATE
-DESCRIPTOR.message_types_by_name['AddFortModifierMessage'] = _ADDFORTMODIFIERMESSAGE
+_UPDATENOTIFICATIONMESSAGE.fields_by_name['state'].enum_type = pogoprotos_dot_enums_dot_notification__state__pb2._NOTIFICATIONSTATE
+DESCRIPTOR.message_types_by_name['UpdateNotificationMessage'] = _UPDATENOTIFICATIONMESSAGE
 
-AddFortModifierMessage = _reflection.GeneratedProtocolMessageType('AddFortModifierMessage', (_message.Message,), dict(
-  DESCRIPTOR = _ADDFORTMODIFIERMESSAGE,
+UpdateNotificationMessage = _reflection.GeneratedProtocolMessageType('UpdateNotificationMessage', (_message.Message,), dict(
+  DESCRIPTOR = _UPDATENOTIFICATIONMESSAGE,
   __module__ = 'pogoprotos.networking.requests.messages.update_notification_message_pb2'
-  # @@protoc_insertion_point(class_scope:pogoprotos.networking.requests.messages.AddFortModifierMessage)
+  # @@protoc_insertion_point(class_scope:pogoprotos.networking.requests.messages.UpdateNotificationMessage)
   ))
-_sym_db.RegisterMessage(AddFortModifierMessage)
+_sym_db.RegisterMessage(UpdateNotificationMessage)
 
 
 # @@protoc_insertion_point(module_scope)
--- pgoapi/protos/pogoprotos/networking/responses/update_notification_response_pb2.py
+++ pgoapi/protos/pogoprotos/networking/responses/update_notification_response_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='pogoprotos/networking/responses/update_notification_response.proto',
   package='pogoprotos.networking.responses',
   syntax='proto3',
-  serialized_pb=_b('\nBpogoprotos/networking/responses/update_notification_response.proto\x12\x1fpogoprotos.networking.responses\x1a)pogoprotos/enums/notification_state.proto\"\x83\x01\n\x16GetHatchedEggsResponse\x12\x18\n\x10notification_ids\x18\x01 \x03(\t\x12\x1b\n\x13\x63reate_timestamp_ms\x18\x02 \x03(\x03\x12\x32\n\x05state\x18\x03 \x01(\x0e\x32#.pogoprotos.enums.NotificationStateb\x06proto3')
+  serialized_pb=_b('\nBpogoprotos/networking/responses/update_notification_response.proto\x12\x1fpogoprotos.networking.responses\x1a)pogoprotos/enums/notification_state.proto\"\x87\x01\n\x1aUpdateNotificationResponse\x12\x18\n\x10notification_ids\x18\x01 \x03(\t\x12\x1b\n\x13\x63reate_timestamp_ms\x18\x02 \x03(\x03\x12\x32\n\x05state\x18\x03 \x01(\x0e\x32#.pogoprotos.enums.NotificationStateb\x06proto3')
   ,
   dependencies=[pogoprotos_dot_enums_dot_notification__state__pb2.DESCRIPTOR,])
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
@@ -28,29 +28,29 @@ _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 
 
-_GETHATCHEDEGGSRESPONSE = _descriptor.Descriptor(
-  name='GetHatchedEggsResponse',
-  full_name='pogoprotos.networking.responses.GetHatchedEggsResponse',
+_UPDATENOTIFICATIONRESPONSE = _descriptor.Descriptor(
+  name='UpdateNotificationResponse',
+  full_name='pogoprotos.networking.responses.UpdateNotificationResponse',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='notification_ids', full_name='pogoprotos.networking.responses.GetHatchedEggsResponse.notification_ids', index=0,
+      name='notification_ids', full_name='pogoprotos.networking.responses.UpdateNotificationResponse.notification_ids', index=0,
       number=1, type=9, cpp_type=9, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='create_timestamp_ms', full_name='pogoprotos.networking.responses.GetHatchedEggsResponse.create_timestamp_ms', index=1,
+      name='create_timestamp_ms', full_name='pogoprotos.networking.responses.UpdateNotificationResponse.create_timestamp_ms', index=1,
       number=2, type=3, cpp_type=2, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='state', full_name='pogoprotos.networking.responses.GetHatchedEggsResponse.state', index=2,
+      name='state', full_name='pogoprotos.networking.responses.UpdateNotificationResponse.state', index=2,
       number=3, type=14, cpp_type=8, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
@@ -69,18 +69,18 @@ _GETHATCHEDEGGSRESPONSE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=147,
-  serialized_end=278,
+  serialized_end=282,
 )
 
-_GETHATCHEDEGGSRESPONSE.fields_by_name['state'].enum_type = pogoprotos_dot_enums_dot_notification__state__pb2._NOTIFICATIONSTATE
-DESCRIPTOR.message_types_by_name['GetHatchedEggsResponse'] = _GETHATCHEDEGGSRESPONSE
+_UPDATENOTIFICATIONRESPONSE.fields_by_name['state'].enum_type = pogoprotos_dot_enums_dot_notification__state__pb2._NOTIFICATIONSTATE
+DESCRIPTOR.message_types_by_name['UpdateNotificationResponse'] = _UPDATENOTIFICATIONRESPONSE
 
-GetHatchedEggsResponse = _reflection.GeneratedProtocolMessageType('GetHatchedEggsResponse', (_message.Message,), dict(
-  DESCRIPTOR = _GETHATCHEDEGGSRESPONSE,
+UpdateNotificationResponse = _reflection.GeneratedProtocolMessageType('UpdateNotificationResponse', (_message.Message,), dict(
+  DESCRIPTOR = _UPDATENOTIFICATIONRESPONSE,
   __module__ = 'pogoprotos.networking.responses.update_notification_response_pb2'
-  # @@protoc_insertion_point(class_scope:pogoprotos.networking.responses.GetHatchedEggsResponse)
+  # @@protoc_insertion_point(class_scope:pogoprotos.networking.responses.UpdateNotificationResponse)
   ))
-_sym_db.RegisterMessage(GetHatchedEggsResponse)
+_sym_db.RegisterMessage(UpdateNotificationResponse)
 
 
 # @@protoc_insertion_point(module_scope)
