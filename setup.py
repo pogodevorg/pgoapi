aaac1bd532477a63db2556adf68b4d1881a70319
Sébastien Vercammen <contact@sebastienvercammen.be>
2018-10-23 15:42:16 +0200

    Fix compatibility w/ latest pip.

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@
 
 import os
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 setup_dir = os.path.dirname(os.path.realpath(__file__))
 path_req = os.path.join(setup_dir, 'requirements.txt')
